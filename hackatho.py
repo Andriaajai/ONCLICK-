97dbc9e7a83e4fdce201c1b97ac84e01dedfa50c
Andriaajai <35872115+Andriaajai@users.noreply.github.com>
2018-03-13 16:29:50 +0530

    Add files via upload

---
+++ hackatho.py
@@ -0,0 +1,40 @@
+import re
+pword=0
+nword=0
+#positive word list
+file = open('positive-words.txt', 'r')
+text = file.read().lower()
+file.close()
+text = re.sub('[^a-z\ \']+', " ", text)
+positive_words = list(text.split())
+#negative word list
+file = open('negative-words.txt', 'r')
+text = file.read().lower()
+file.close()
+text = re.sub('[^a-z\ \']+', " ", text)
+negative_words = list(text.split())
+print(negative_words)
+#para break
+listofwords=[]
+sentiment_val=0
+print("Program to review teacher feedback")
+file = open('review.txt', 'r')
+text = file.read().lower()
+file.close()
+text = re.sub('[^a-z\ \']+', " ", text)
+review= list(text.split())
+print(review)
+for word in listofwords:
+	if(word in positive_words):
+		sentiment_val=sentiment_val+1
+		pword=pword+1
+	if(word in negative_words):
+		sentiment_val=sentiment_val-1
+		nword=nword-1
+
+if(sentiment_val>0):
+	print('Well done')
+else:
+	print('Needs improvement')
+print(pword)
+print(nword)
